7ca1be241b6d9a1beba49fd0baf200a9015a3767
EC2 Default User <ec2-user@ip-172-31-44-69.eu-west-1.compute.internal>
2024-07-28 19:42:19 +0000

    updated urls.py

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -1,3 +1,9 @@
+"""
+URL configuration for the Django project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/stable/topics/http/urls/
+"""
 from django.contrib import admin
 from django.urls import path, include
 
