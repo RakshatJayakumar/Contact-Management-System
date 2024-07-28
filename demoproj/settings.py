f7348097fa9142aeb28e262dec139055abf8eb50
EC2 Default User <ec2-user@ip-172-31-44-69.eu-west-1.compute.internal>
2024-07-28 20:01:59 +0000

    updated main.html and settings.py

--- demoproj/settings.py
+++ demoproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2+s9#g5@-1a1a@l@18($++_*)(-l-z8jl(q6t69!h7xvjzgmz('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
